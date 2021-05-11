044646a53d0f2514dad5b06a452bab3715aa8a0e
SharaevOleg <61539375+SharaevOleg@users.noreply.github.com>
2021-05-11 10:12:01 +0300

    Add files via upload

---
+++ src/main/java/web/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import web.config.handler.LoginSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    private UserService userService;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+//    @Autowired
+//    public void setUserService(UserService userService) {
+//        this.userService = userService;
+//    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/login").anonymous()
+                .antMatchers("/admin").hasAuthority("ADMIN")
+                .antMatchers("/users").hasAnyAuthority("ADMIN", "USER")
+                .anyRequest().authenticated().and().formLogin().successHandler(new LoginSuccessHandler());
+
+        httpSecurity.logout().permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").and().csrf().disable();
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,66 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+//@RequestMapping("/")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = {"/", "/users"})
+    public String getUser(ModelMap model) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("users", userDetails);
+        return "users";
+    }
+
+    @GetMapping(value = "/admin")
+    public String allUsers(ModelMap model) {
+        model.addAttribute("listUsers", userService.getAllUsers());
+        model.addAttribute("allRoles", userService.getRole());
+        model.addAttribute("newUser", new User());
+        return "admin";
+    }
+
+    @PostMapping(value = "/createUser")
+    public String createUser(@ModelAttribute("user") User user) {
+        userService.create(user);
+        return "redirect:/admin";
+    }
+
+    @PostMapping(value = "/updateUser/{id}")
+    public String updateUser(ModelMap model, @PathVariable("id") Long id) {
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+        model.addAttribute("allRoles", userService.getRole());
+        return "updateUser";
+    }
+
+    @PostMapping(value = "/updateUser")
+    public String update(@ModelAttribute("user") User user) {
+        userService.update(user);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteUserById(@PathVariable("id") Long id) {
+        userService.remove(id);
+        return "redirect:/admin";
+    }
+
+
+
+}
